c7c3ae82e342f0c9cc9c9b65decc6d7c44c6f215
BrunaLopesGois <bruna-chan@live.com>
2021-02-12 17:07:04 -0300

    changing route methods

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,6 +2,7 @@
 <template>
   <div>
     <h1 class="centralizado">{{ titulo }}</h1>
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
     <input
       type="search"
       class="filtro"
@@ -33,6 +34,7 @@
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
 import Botao from "../shared/botao/Botao.vue";
+import FotoService from "../../domain/foto/FotoService";
 
 export default {
   components: {
@@ -46,6 +48,7 @@ export default {
       titulo: "AluraPic",
       fotos: [],
       filtro: "",
+      mensagem: "",
     };
   },
 
@@ -62,14 +65,24 @@ export default {
 
   methods: {
     remove(foto) {
-      alert(`Remover a foto ${foto.titulo}`);
+      this.service.apaga(foto._id).then(
+        () => {
+          let indice = this.fotos.indexOf(foto);
+          this.fotos.splice(indice, 1);
+          this.mensagem = "Foto removida com sucesso";
+        },
+        (err) => {
+          console.log(err), (this.mensagem = "Não foi possível remover a foto");
+        }
+      );
     },
   },
 
   created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then((res) => res.json())
+    this.service = new FotoService(this.$resource);
+
+    this.service
+      .lista()
       .then((fotos) => ((this.fotos = fotos), (err) => console.log(err)));
   },
 };
