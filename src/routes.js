d2349f59d1de20610ef5093612d4efb755c952f4
BrunaLopesGois <bruna-chan@live.com>
2021-02-16 15:06:17 -0300

    distributing application

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
 import Home from './components/home/Home.vue';
+
 export const routes = [
   { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
   { path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
